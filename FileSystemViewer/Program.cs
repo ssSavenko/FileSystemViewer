d3abe6a9476ac7d6bb2170329c6dec41c6cb0536
ss.savenko <ss.savenko@gmail.com>
2018-06-04 22:34:37 +0300

    Add project files.

---
+++ FileSystemViewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSystemViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
